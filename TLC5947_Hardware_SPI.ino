f94f5ca2ebf230da2313cbdfdc232e086dd4c85c
PIMS_DESKTOP\mail <contact@pimswinkels.com>
2024-04-24 12:44:46 +0200

    Made the example sketch run around RGBW leds instead of single led dots

--- TLC5947_Hardware_SPI.ino
+++ TLC5947_Hardware_SPI.ino
@@ -10,53 +10,54 @@
 #define CLK_PIN 18
 
 // hardware setup:
-#define N_LEDDOTS 25
+#define N_LEDS 24
 
 // SPI Config:
 #define SPI_FREQ 1000000 // 10MHz
 
+// create the RGBW led struct
+struct RGBWLed {
+    uint16_t r;
+    uint16_t g;
+    uint16_t b;
+    uint16_t w;
+};
+
 // led data:
-uint16_t leds[N_LEDDOTS];
+RGBWLed leds[N_LEDS];
 
 // create the TLC5947 object:
 // The led array needs to be initialized prior to this object creation
-TLC5947 ledDriver(leds, N_LEDDOTS, DATA_PIN, CLK_PIN, LATCH_PIN);
+TLC5947 ledDriver((uint16_t*)leds, N_LEDS*(sizeof(leds[0])/sizeof(leds[0].r)), DATA_PIN, CLK_PIN, LATCH_PIN);
 
 uint32_t loopCounter = 0;
+
+void setAllLedsTo(struct RGBWLed color)
+{
+    for (int i = 0; i < N_LEDS; i++) {
+        memcpy(&leds[i], &color, sizeof(leds[0]));
+    }
+}
 
 void setup()
 {
     Serial.begin(115200);
+    pinMode(OE_PIN, OUTPUT);
+    digitalWrite(OE_PIN, LOW);
 }
 
 void loop()
 {
-    uint32_t start = micros();
-    setAllLedsTo(0xFF);
-    uint32_t updateStart = micros();
+    setAllLedsTo({ 4095, 0, 0, 0 });
     ledDriver.update();
-    uint32_t clearStart = micros();
-    clearLeds();
-    uint32_t secondUpdateStart = micros();
+    delay(500);
+    setAllLedsTo({ 0, 4095, 0, 0 });
     ledDriver.update();
-    uint32_t secondUpdateDone = micros();
-
-    if (loopCounter % 1000 == 0) {
-        Serial.println("All Leds to full: " + String(updateStart - start) + "us\nFirst update: " + String(clearStart - updateStart) + "us\nAll leds off: " + String(secondUpdateStart - clearStart) + "us\nsecondUpdate: " + String(secondUpdateDone - secondUpdateStart) + "us\n\n");
-    }
-    loopCounter++;
-}
-
-void clearLeds()
-{
-    for (int i = 0; i < N_LEDDOTS; i++) {
-        leds[i] = 0;
-    }
-}
-
-void setAllLedsTo(uint16_t brightness)
-{
-    for (int i = 0; i < N_LEDDOTS; i++) {
-        leds[i] = brightness;
-    }
+    delay(500);
+    setAllLedsTo({ 0, 0, 4095, 0 });
+    ledDriver.update();
+    delay(500);
+    setAllLedsTo({ 0, 0, 0, 4095 });
+    ledDriver.update();
+    delay(500);
 }
