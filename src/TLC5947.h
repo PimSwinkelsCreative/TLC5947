9c7d3072d62d861aead390bde9f3162b078c955c
Pim Swinkels <75797317+PimSwinkelsCreative@users.noreply.github.com>
2024-09-10 10:38:33 +0200

    Updated the warnings to be optional (they are on by default)

--- src/TLC5947.h
+++ src/TLC5947.h
@@ -1,46 +1,55 @@
 #pragma once
-#include <Arduino.h>
 #include <16bitPixelTypes.h>
+#include <Arduino.h>
 
 #define BYTESPERDRIVER 36
 #define LEDDOTSPERDRIVER 24
 
-#define DEFAULT_SPI_CLK 10000000 // 10MHz
-
+#define DEFAULT_SPI_CLK 10000000  // 10MHz
 
 class TLC5947 {
-private:
-    uint16_t* leds;
-    uint16_t nLedDots;
-    uint8_t clkPin;
-    uint8_t dataPin;
-    uint8_t latchPin;
-    int8_t blankPin;
-    uint32_t clkFrequency;
-    uint8_t nLedDrivers;
+ private:
+  uint16_t* leds;
+  uint16_t nLedDots;
+  uint8_t clkPin;
+  uint8_t dataPin;
+  uint8_t latchPin;
+  int8_t blankPin;
+  uint32_t clkFrequency;
+  uint8_t nLedDrivers;
+  bool disableWarnings;
 
-    void init();
-    void printOutOfRangeError();
+  void init();
+  void printOutOfRangeError();
 
-public:
-    // constructors:
-    TLC5947(RGBColor16* _rgbLedData, uint16_t _nRGBLeds, uint8_t _clkPin, uint8_t _dataPin, uint8_t _latchPin, int8_t _blankPin = -1, uint32_t _clkFrequency = DEFAULT_SPI_CLK);
-    TLC5947(RGBWColor16* _rgbwLedData, uint16_t _nRGBWLeds, uint8_t _clkPin, uint8_t _dataPin, uint8_t _latchPin, int8_t _blankPin = -1, uint32_t _clkFrequency = DEFAULT_SPI_CLK);
-    TLC5947(uint16_t* _ledData, uint16_t _nLedDots, uint8_t _clkPin, uint8_t _dataPin, uint8_t _latchPin, int8_t _blankPin = -1, uint32_t _clkFrequency = DEFAULT_SPI_CLK);
+ public:
+  // constructors:
+  TLC5947(RGBColor16* _rgbLedData, uint16_t _nRGBLeds, uint8_t _clkPin,
+          uint8_t _dataPin, uint8_t _latchPin, int8_t _blankPin = -1,
+          uint32_t _clkFrequency = DEFAULT_SPI_CLK,
+          bool _disableWarnings = false);
+  TLC5947(RGBWColor16* _rgbwLedData, uint16_t _nRGBWLeds, uint8_t _clkPin,
+          uint8_t _dataPin, uint8_t _latchPin, int8_t _blankPin = -1,
+          uint32_t _clkFrequency = DEFAULT_SPI_CLK,
+          bool _disableWarnings = false);
+  TLC5947(uint16_t* _ledData, uint16_t _nLedDots, uint8_t _clkPin,
+          uint8_t _dataPin, uint8_t _latchPin, int8_t _blankPin = -1,
+          uint32_t _clkFrequency = DEFAULT_SPI_CLK,
+          bool _disableWarnings = false);
 
-    // setting led:
-    void setLedTo(uint16_t ledIndex, struct RGBWColor16 color);
-    void setLedTo(uint16_t ledIndex, struct RGBColor16 color);
-    void setLedTo(uint16_t ledIndex, uint16_t brightness);
+  // setting led:
+  void setLedTo(uint16_t ledIndex, struct RGBWColor16 color);
+  void setLedTo(uint16_t ledIndex, struct RGBColor16 color);
+  void setLedTo(uint16_t ledIndex, uint16_t brightness);
 
-    // setting all leds:
-    void setAllLedsTo(struct RGBWColor16 color);
-    void setAllLedsTo(struct RGBColor16 color);
-    void setAllLedsTo(uint16_t brightness);
+  // setting all leds:
+  void setAllLedsTo(struct RGBWColor16 color);
+  void setAllLedsTo(struct RGBColor16 color);
+  void setAllLedsTo(uint16_t brightness);
 
-    // clear all leds:
-    void clearLeds();
+  // clear all leds:
+  void clearLeds();
 
-    // write the ledData to the led drivers:
-    void update();
+  // write the ledData to the led drivers:
+  void update();
 };
